45c709dfc38ef4a03453c3cdb11c1a5efaa0041f
takeshi.ootsuka <takeshi.otsuka111@gmail.com>
2021-01-20 16:30:50 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by user on 2021/01/20.
+//  Copyright © 2021 takeshi-2983. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
